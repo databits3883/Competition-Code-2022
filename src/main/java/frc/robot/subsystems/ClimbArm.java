60bd400f9aea743125ff226a4f3af8189e2a724b
alex-vanasse <49699649+alex-vanasse@users.noreply.github.com>
2022-01-22 10:57:28 -0600

    Added controllers and actuators to climArm

--- src/main/java/frc/robot/subsystems/ClimbArm.java
+++ src/main/java/frc/robot/subsystems/ClimbArm.java
@@ -4,9 +4,27 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbArm extends SubsystemBase {
+  final MotorController m_armLengthWinchMotor;
+  final MotorController m_armTiltWinchMotor;
+  final Encoder m_armLengthEncoder;
+  final Encoder m_armTiltEncoder;
+
+  final ArmFeedforward m_unloadedArmController;
+  final PIDController m_armAngleAdjustor;
+
+  final ElevatorFeedforward m_armLenghtController;
+  final PIDController m_armLengthAdjustor;
+
+
+
   /** Creates a new ClimbArm. */
   public ClimbArm() {}
 
