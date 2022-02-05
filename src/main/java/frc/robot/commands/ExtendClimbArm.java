3c0282c59ec6a416d65efbaeb293f5d0921ad1c8
James W - PC 4 <=>
2022-02-05 14:20:15 -0600

    Added a command to extend the climb arms a specified distance. Also fixed a bu in the intake command.

--- src/main/java/frc/robot/commands/ExtendClimbArm.java
+++ src/main/java/frc/robot/commands/ExtendClimbArm.java
@@ -5,47 +5,45 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.ClimbArm;
 
-public class RunIntake extends CommandBase {
+public class ExtendClimbArm extends CommandBase {
 
-    private final Intake m_intake;
-    private final int m_direction;
+  private double m_distanceToExtend;
+  private ClimbArm m_arms;
+  private double m_startDistance;
 
-  /** Creates a new RunIntake. */
-  public RunIntake(Intake intake, int direction) {
-    m_intake = intake;
-    m_direction = direction;
+  /** Creates a new ExtendClimbArm. */
+  public ExtendClimbArm(double distance, ClimbArm arms) {
+
+    m_distanceToExtend = distance;
+    m_arms = arms;
+    m_startDistance = m_arms.measureArmLength();
 
     // Use addRequirements() here to declare subsystem dependencies.
-
-    addRequirements(m_intake);
+    addRequirements(m_arms);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_intake.takeInOurOut(Constants.IntakeConstants.spinSpeed * m_direction);
+    m_arms.setExtensionSpeed(0.5);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_intake.takeInOurOut(Constants.IntakeConstants.spinSpeed * m_direction);
-
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_intake.takeInOurOut(0.0);
-
+    m_arms.setExtensionSpeed(0.0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+
+    return m_arms.measureArmLength() < m_distanceToExtend + m_startDistance;
   }
 }
