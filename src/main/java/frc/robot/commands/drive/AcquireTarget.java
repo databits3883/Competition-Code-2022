e25241a145154d7f979c889d88c7f971b84af3ab
frc3883 <3883outreach@gmail.com>
2022-03-19 13:49:31 -0500

    added target speed model

--- src/main/java/frc/robot/commands/drive/AcquireTarget.java
+++ src/main/java/frc/robot/commands/drive/AcquireTarget.java
@@ -7,20 +7,41 @@ package frc.robot.commands.drive;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.commands.autonomous.Tracking.TurnToGoal;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Launcher;
 import frc.robot.subsystems.Vision;
 
 public class AcquireTarget extends TurnToGoal {
 
+  Launcher m_launcher;
+  Vision m_vision;
+
+  NetworkTableEntry m_validSpeeEntry;
   /** Creates a new AcquireTarget. */
-  public AcquireTarget(Joystick stick, Drivetrain drivetrain, Vision vision) {
+  public AcquireTarget(Joystick stick, Drivetrain drivetrain, Vision vision, Launcher launcher) {
     // Use addRequirements() here to declare subsystem dependencies.
     super(drivetrain, vision);
+    m_launcher = launcher;
+    m_vision = vision;
+    m_validSpeeEntry = NetworkTableInstance.getDefault().getTable("autoAim").getEntry("speed valid");
+    m_validSpeeEntry.setBoolean(false);
+    addRequirements(launcher);
   }
   
+  @Override
+  public void execute(){
+    super.execute();
+    double setSpeed = 86.7*m_vision.getDistanceVision() + 1318;
+
+    double correctedSpeed = Math.min(1900, Math.max(1500, setSpeed));
+    m_validSpeeEntry.setBoolean(correctedSpeed == setSpeed);
+    if(m_vision.isTargetValid()) m_launcher.SetShooterSpeed(correctedSpeed);
+  }
   @Override
   public ChassisSpeeds getTranslationalSpeed(){
     return JoystickDrive.StickFilter.getCurrentCommand();
